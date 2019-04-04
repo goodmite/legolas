bd59cd4bd2f1069542ec677281ecf4d9e97eeb2e
Sandeep Kumar Gupta <sandeepkumar.g@imimobile.com>
2019-04-05 02:43:19 +0530

    feature(hmr): done

---
+++ src/environments/environment.hmr.ts
@@ -0,0 +1,6 @@
+export const environment = {
+    production: false,
+    hmr: true,
+    backend_root:'http://52.41.160.116',
+    'API_KEY':'54asdkj1209nksnda'
+};
--- src/main.ts
+++ src/main.ts
@@ -8,10 +8,27 @@ import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 
 import { AppModule } from './app/app.module';
 import { environment } from './environments/environment';
+import {hmrModule} from "@angularclass/hmr";
 
 if (environment.production) {
   enableProdMode();
 }
 
-platformBrowserDynamic().bootstrapModule(AppModule)
-  .catch(err => console.error(err));
+// platformBrowserDynamic().bootstrapModule(AppModule)
+//   .catch(err => console.error(err));
+
+const bootstrap = () => platformBrowserDynamic().bootstrapModule(AppModule);
+
+if ((<any>environment).hmr) {
+  if (module[ 'hot' ]) {
+    // hmrBootstrap(module, bootstrap);
+    bootstrap().then((ngModuleRef) => {
+      return hmrModule(ngModuleRef, module);
+    }).catch(err => console.log(err));
+  } else {
+    console.error('HMR is not enabled for webpack-dev-server!');
+    console.log('Are you using the --hmr flag for ng serve?');
+  }
+} else {
+  bootstrap().catch(err => console.log(err));
+}
